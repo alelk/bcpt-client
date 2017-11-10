fed6c7d28717593bb70707c47a91f835de0a1fbd
Alex Elkin <alelkdev@gmail.com>
2017-11-10 08:00:56 +0300

    Working on Importer functionality: importer reducers have been added. Changes in the importer api.

--- src/api/bcptImporterApi.js
+++ src/api/bcptImporterApi.js
@@ -7,8 +7,15 @@
 import BcptConfig from '../util/BcptConfig'
 import {postObject} from './fetchFunctions'
 
+import { schema } from 'normalizr'
 import urlencode from 'urlencode'
 
+const importProcessSchema = new schema.Entity('imports', {}, {
+    idAttribute : value => value.importProcessId,
+    processStrategy : value => Object.assign({}, value, {fileName : urlencode.decode(value.fileName)})
+});
+const importProcessesSchema = new schema.Array(importProcessSchema);
+
 export const importFile = (category, fileName) => {
-    return postObject(BcptConfig.get("rest-api-uri") + "import/" + category + "?fileName=" + urlencode(fileName));
+    return postObject(BcptConfig.get("rest-api-uri") + "import/" + category + "?fileName=" + urlencode(fileName), undefined, importProcessSchema);
 };
